c29297e45915c3f37db2d65aa05040e15969d0c3
LAPTOP-4UDAE21C\VAMSHI <vamshivujini3108@gamil.com>
2021-08-28 11:44:23 +0530

    Initial commit

---
+++ Assets/movments.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class movments : MonoBehaviour
+{
+	public  Transform player;
+
+	public float speed;
+	public float RotateSpeed = 30f;
+
+	void Update()
+	{
+		Debug.Log (Time.deltaTime);
+		if (Input.GetKey(KeyCode.W)) 
+		{
+			player.position = new Vector3 (player.position.x + speed*Time.deltaTime, player.position.y, player.position.z);
+		}
+		if (Input.GetKey(KeyCode.S)) 
+		{
+			player.position = new Vector3 (player.position.x - speed*Time.deltaTime, player.position.y, player.position.z);
+		}
+		if (Input.GetKey(KeyCode.D)) 
+		{
+			player.position = new Vector3 (player.position.x,player.position.y,player.position.z + speed*Time.deltaTime);
+
+		}
+		if (Input.GetKey(KeyCode.A)) 
+		{
+			player.position = new Vector3 (player.position.x,player.position.y,player.position.z - speed*Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.Space)) 
+		{
+			player.position = new Vector3 (player.position.x,player.position.y + speed*Time.deltaTime,player.position.z);
+		}
+		if (Input.GetKey (KeyCode.C)) 
+		{
+			player.position = new Vector3 (player.position.x,player.position.y - speed*Time.deltaTime,player.position.z);
+		}
+		if (Input.GetKey(KeyCode.P))
+		{
+			player.localScale = new Vector3(player.localScale.x + 1, player.localScale.y, player.localScale.z);
+		}
+		if (Input.GetKey(KeyCode.O))
+		{
+			player.localScale = new Vector3(player.localScale.x , player.localScale.y + 1, player.localScale.z);
+		}
+		
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			player.Rotate(-Vector3.up * RotateSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			player.Rotate(Vector3.up * RotateSpeed * Time.deltaTime);
+		}
+	}
+}
